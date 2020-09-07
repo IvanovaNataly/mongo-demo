c5b73d459be44946face1e5a3c264fc6a42314f9
IvanovaNataly <letter4natalia@gmail.com>
2020-09-07 14:12:02 +0300

    schema, model, create device

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 // const userSchema = require('./userSchema.js')
 // const User = mongoose.model('user', userSchema, 'user')
 const connectionString = 'mongodb+srv://admin:1qa2ws3ed@cluster0.bfdos.mongodb.net/dbDefault?retryWrites=true&w=majority'
 
 mongoose.connect(connectionString)
 	.then(() => console.log('connected'))
-	.catch(err => console.error('could not connect', err))
+	.catch(err => console.error('could not connect', err));
+
+const deviceSchema = new mongoose.Schema({
+	primaryKey: { leadingKey: Number, secondaryKey: Number },
+	name: String,
+	type: String
+});
+
+const Device = mongoose.model('Device', deviceSchema);
+
+async function createDevice() {
+	const sensor1 = new Device({
+		primaryKey: { leadingKey: 1, secondaryKey: 2 },
+		name: 'Sensor1',
+		type: 'Sensor'
+	});
+
+	const result = await sensor1.save();
+	console.log(result);
+}
+
+createDevice();
+
 //
 // async function createUser(username) {
 // 	return new User({
